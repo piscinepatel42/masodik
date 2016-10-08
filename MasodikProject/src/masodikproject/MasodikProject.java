0b6ebf44c5831376360fb970d2eac5b317913739
piscinepatel42 <piscinepatel42@gmail.com>
2016-10-08 15:32:23 +0200

    create project
    create project

---
+++ MasodikProject/src/masodikproject/MasodikProject.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package masodikproject;
+
+/**
+ *
+ * @author Brigi
+ */
+public class MasodikProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello1");
+        System.out.println("hello12");
+        System.out.println("hello13");
+        System.out.println("hello13");
+        System.out.println("hello14");
+        System.out.println("hello15");
+    }
+    
+}
