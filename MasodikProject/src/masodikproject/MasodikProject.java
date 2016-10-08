941c148a0202f9f3ecdfa73b7ef8ce6dbb6361e6
piscinepatel42 <piscinepatel42@gmail.com>
2016-10-08 17:56:59 +0200

    add 23 and 24
    add 23 and 24

--- MasodikProject/src/masodikproject/MasodikProject.java
+++ MasodikProject/src/masodikproject/MasodikProject.java
@@ -26,7 +26,10 @@ public class MasodikProject {
         System.out.println("hello19");      
         System.out.println("hello20");     
         System.out.println("hello21");     
-        System.out.println("hello22");      
+        System.out.println("hello22");   
+        System.out.println("hello23");     
+        System.out.println("hello24");
+        
     }
 	
     static void writeOne(){
